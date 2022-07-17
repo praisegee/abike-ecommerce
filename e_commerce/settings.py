b060590365536eb02d95d2146ad2c6e8d30405f2
Adesanmi Dayo PraiseGod <dayopraisegod@gmail.com>
2022-07-17 18:35:22 +0100

    new

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -49,6 +49,7 @@ AUTH_USER_MODEL = "account.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
